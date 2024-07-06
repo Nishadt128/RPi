3d4a97320cf9af725904b34e89256dca0e131e8c
Nishadt128 <161719408+Nishadt128@users.noreply.github.com>
2024-07-06 14:14:46 +0100

    Create DS3231.h
    Created header file for DS32331 c file

---
+++ DS3231.h
@@ -0,0 +1,26 @@
+#ifndef DS3231_H
+#define DS3231_H
+
+#include "I2CDevice.h"
+#include <cstdint>
+
+class DS3231 : public EE513::I2CDevice {
+public:
+    DS3231(unsigned int bus, unsigned int device);
+    void readTime();
+    void printTime();
+    void setTime(uint8_t seconds, uint8_t minutes, uint8_t hours, uint8_t dayOfWeek, uint8_t dayOfMonth, uint8_t month, uint8_t year);
+
+private:
+    uint8_t bcdToDec(uint8_t bcd);
+    
+    uint8_t seconds;
+    uint8_t minutes;
+    uint8_t hours;
+    uint8_t dayOfWeek;
+    uint8_t dayOfMonth;
+    uint8_t month;
+    uint8_t year;
+};
+
+#endif
