597179df33690d95341a673e21ff3c89aaa9c39c
Nishadt128 <161719408+Nishadt128@users.noreply.github.com>
2024-07-14 18:03:51 +0100

    Update DS3231.h

--- DS3231.h
+++ DS3231.h
@@ -12,15 +12,9 @@ public:
     void setTime(uint8_t seconds, uint8_t minutes, uint8_t hours, uint8_t dayOfWeek, uint8_t dayOfMonth, uint8_t month, uint8_t year);
 
 private:
+    uint8_t seconds, minutes, hours, dayOfWeek, dayOfMonth, month,year;
     uint8_t bcdToDec(uint8_t bcd);
     uint8_t decToBcd(uint8_t dec);
-    uint8_t seconds;
-    uint8_t minutes;
-    uint8_t hours;
-    uint8_t dayOfWeek;
-    uint8_t dayOfMonth;
-    uint8_t month;
-    uint8_t year;
 };
 
 #endif
