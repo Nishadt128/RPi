f5428173304217ea3c7afd7b5c39153cc85b8d48
Nishadt128 <161719408+Nishadt128@users.noreply.github.com>
2024-07-14 18:09:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,70 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void displayCurrentTime(DS3231& rtc){
+	while (true){
+	rtc.printTime();
+	this_thread::sleep_for(chrono::seconds(1));
+	cout << "\033[2J\033[1;1H";	
+	}
+}
+
+void setCurrentTime(DS3231& rtc) {
+    uint8_t seconds, minutes, hours, dayOfWeek, dayOfMonth, month, year;
+
+    cout << "Enter seconds: ";
+    cin >> seconds;
+    cout << "Enter minutes: ";
+    cin >> minutes;
+    cout << "Enter hours: ";
+    cin >> hours;
+    cout << "Enter day of the week (1=Sun, 2=Mon, ... 7=Sat): ";
+    cin >> dayOfWeek;
+    cout << "Enter day of the month: ";
+    cin >> dayOfMonth;
+    cout << "Enter month: ";
+    cin >> month;
+    cout << "Enter year (last two digits): ";
+    cin >> year;
+
+    rtc.setTime(seconds, minutes, hours, dayOfWeek, dayOfMonth, month, year);
+}
+
+void displaySetTime(DS3231& rtc) {
+    rtc.printTime();
+}
 
 int main(){
 	DS3231 rtc(1, 0x68);
-	rtc.setTime(0,45,20,4,3,7,24);
+	int choice;
 	while (true) {
-        rtc.printTime();
-        std::this_thread::sleep_for(std::chrono::seconds(1)); // Update every second
-        std::cout << "\033[2J\033[1;1H"; // Clear the console
-    }	
+        cout << "1. Display current time\n";
+	cout << "2. Set current time\n";
+	cout << "3. Display set time\n";
+	cout << "4. Exit\n";
+	cout << "Enter your choice";
+	cin >> choice;
+	
+	switch (choice){
+	case 1:
+	displayCurrentTime(rtc);
+	break;
+	case 2:
+	setCurrentTime(rtc);
+	break;
+	case 3:
+	displaySetTime(rtc);
+	break;
+	case 4:
 	return 0;
+	default:
+	cout << "Invalid choice. Please try again.\n";
+	}
+    }	
+return 0;
 }
+
