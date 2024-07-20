eefba9b141238a1c05536c300532209c8c2209e4
Nishadt128 <161719408+Nishadt128@users.noreply.github.com>
2024-07-20 22:46:42 +0100

    Update main.cpp
    Added alarms and temperature features

--- main.cpp
+++ main.cpp
@@ -3,36 +3,47 @@
 #include <thread>
 #include <iostream>
 #include <iomanip>
+#include <atomic>
 
 using namespace std;
+
+atomic<bool> running(true);
 
 void displayCurrentTime(DS3231& rtc){
 	while (true){
 	rtc.printTime();
 	this_thread::sleep_for(chrono::seconds(1));
-	cout << "\033[2J\033[1;1H";	
+	// cout << "\033[2J\033[1;1H";	
 	}
 }
 
 void setCurrentTime(DS3231& rtc) {
-    uint8_t seconds, minutes, hours, dayOfWeek, dayOfMonth, month, year;
+   uint8_t seconds, minutes, hours, dayOfWeek, dayOfMonth, month, year;
 
-    cout << "Enter seconds: ";
-    cin >> seconds;
-    cout << "Enter minutes: ";
-    cin >> minutes;
-    cout << "Enter hours: ";
-    cin >> hours;
-    cout << "Enter day of the week (1=Sun, 2=Mon, ... 7=Sat): ";
-    cin >> dayOfWeek;
-    cout << "Enter day of the month: ";
-    cin >> dayOfMonth;
-    cout << "Enter month: ";
-    cin >> month;
-    cout << "Enter year (last two digits): ";
-    cin >> year;
+    seconds = 0;
+    minutes = 15 ;hours= 20;
+    dayOfWeek = 7; 
+    dayOfMonth = 20;
+    month = 7;
+    year = 24;
 
     rtc.setTime(seconds, minutes, hours, dayOfWeek, dayOfMonth, month, year);
+}
+
+void setAlarm1(DS3231& rtc){
+    cout << "Setting alarm 1 for 1 minute" << endl;
+    rtc.setAlarm1(0,1,0);
+    cout << "Alarm 1 set" <<endl;
+}
+
+void setAlarm2(DS3231& rtc){
+    cout<< "Setting Alarm 2 for 2 mintues"<<endl;
+    rtc.setAlarm2(2,0);
+    cout << "Alarm 2 set" << endl;
+}
+
+void displayTemperature(DS3231& rtc){
+	rtc.printTemperature();
 }
 
 void displaySetTime(DS3231& rtc) {
@@ -41,31 +52,33 @@ void displaySetTime(DS3231& rtc) {
 
 int main(){
 	DS3231 rtc(1, 0x68);
-	int choice;
+	char option;
+	cout << "Select an option: \n1. Display current time\n2. Set current time\n3. Set Alarm 1\n4. Set Alarm 2\n5. Display Temperature\nq. Quit"<<endl;
 	while (true) {
-        cout << "1. Display current time\n";
-	cout << "2. Set current time\n";
-	cout << "3. Display set time\n";
-	cout << "4. Exit\n";
-	cout << "Enter your choice";
-	cin >> choice;
+        cin>>option;
+	if (option == 'q') break;
 	
-	switch (choice){
-	case 1:
-	displayCurrentTime(rtc);
-	break;
-	case 2:
-	setCurrentTime(rtc);
-	break;
-	case 3:
-	displaySetTime(rtc);
-	break;
-	case 4:
-	return 0;
-	default:
-	cout << "Invalid choice. Please try again.\n";
-	}
-    }	
+	switch (option){
+		case '1':
+			displayCurrentTime(rtc);
+			break;
+		case '2':
+			setCurrentTime(rtc);
+			break;
+		case '3':
+			setAlarm1(rtc);
+			break;
+		case '4':
+			setAlarm2(rtc);
+			break;
+		case '5':
+			displayTemperature(rtc);
+			break;
+		default:
+			cout << "Invalid choice. Please try again.\n";
+		}
+    }
+	running = false;
 return 0;
 }
 
