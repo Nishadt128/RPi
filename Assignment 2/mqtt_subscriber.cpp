323e5953918dd1f5f4386f23f14f76b4f2c7ff96
Nishadt128 <161719408+Nishadt128@users.noreply.github.com>
2024-07-28 17:43:31 +0100

    Update mqtt_subscriber.cpp

--- Assignment 2/mqtt_subscriber.cpp
+++ Assignment 2/mqtt_subscriber.cpp
@@ -6,7 +6,7 @@
   #define CLIENTID "rpi2"
   #define AUTHMETHOD "user1"
   #define AUTHTOKEN "user1"
-  #define TOPIC "Sensor/Temp"
+  #define TOPIC "Sensor/data"
   #define PAYLOAD "Hello World!"
   #define QOS 1
   #define TIMEOUT 10000L
